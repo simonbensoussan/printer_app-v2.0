437610953024922cea9682cc467b9974a65f4754
simon <simonbensoussan.sb@gmail.com>
2017-07-25 15:42:24 +0000

    set up views_generic REST API with view generic works !

--- landing/urls.py
+++ landing/urls.py
@@ -9,16 +9,16 @@ from . import views_generic
 urlpatterns = [
     url(r'^stocks-test/$', views_generic.CreateView.as_view(), name="create"),
     url(r'^stocks-test/(?P<pk>[0-9]+)$', views_generic.StocksDetail.as_view(), name="get_delete_update_stock"),
-    # url(
-    #     r'^stocks/(?P<pk>[0-9]+)$',
-    #     views.get_delete_update_stock,
-    #     name='get_delete_update_stock'
-    # ),
-    # url(
-    #     r'^stocks/$',
-    #     views.get_post_stock,
-    #     name='get_post_stock'
-    # )
+    url(
+        r'^stocks/(?P<pk>[0-9]+)$',
+        views.get_delete_update_stock,
+        name='get_delete_update_stock'
+    ),
+    url(
+        r'^stocks/$',
+        views.get_post_stock,
+        name='get_post_stock'
+    )
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
