54319aba80a4a9b3d36f1509e57a2e328e2e77f2
simon <simonbensoussan.sb@gmail.com>
2017-07-25 14:05:44 +0000

    update model add dateTime field + verbose plural

--- landing/admin.py
+++ landing/admin.py
@@ -4,6 +4,6 @@ from .models import Stocks
 
 class StockAdmin(admin.ModelAdmin):
     """Model admin for interface admin"""
-    list_display = ('name','open','volume')
+    list_display = ('name','open','volume','created_at','updated_at')
     
 admin.site.register(Stocks,StockAdmin)
--- landing/models.py
+++ landing/models.py
@@ -1,14 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
-
+def default_time():
+    """set default timezone for migrations"""
+    return timezone.now()
+    
 class Stocks(models.Model):
     name = models.CharField(max_length=250)
     open = models.FloatField()
     volume = models.IntegerField()
-    
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=False,default=default_time)
+
+    class Meta:
+        verbose_name_plural = 'Mes Stocks'
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return '{}'.format(self.name)
